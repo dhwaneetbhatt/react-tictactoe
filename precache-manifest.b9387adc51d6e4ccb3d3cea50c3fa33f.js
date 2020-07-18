f351c98c0a5704413d63baa4d71593b0b67abb14
dhwaneetbhatt <dhwaneetbhatt@gmail.com>
2020-07-18 20:24:55 +0530

    Updates

---
+++ precache-manifest.b9387adc51d6e4ccb3d3cea50c3fa33f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9d2a4acd1a57cc2074171a1ff00e06",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "9a11f183433e46c47424",
+    "url": "/react-tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "68ec24540e97e58279d6",
+    "url": "/react-tictactoe/static/js/2.eca63b7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.eca63b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a11f183433e46c47424",
+    "url": "/react-tictactoe/static/js/main.39b7b9ce.chunk.js"
+  },
+  {
+    "revision": "70f3ab98cadcbc29ce8e",
+    "url": "/react-tictactoe/static/js/runtime-main.9d689b68.js"
+  }
+]);
